59b89a1346d41983ba5dee5ba49dc0ca22bbaee8
osvaldo hinojosa <osvaldo.javier14@gmail.com>
2024-08-16 19:40:23 -0600

    add postgresl

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calcultaro functions
+"""
+
+
+def add(x, y):
+    """Add two numbers together"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return value"""
+    return y - x
